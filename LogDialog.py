7fb592707ace906c47722bea0ea780c100c4f186
informaticosonline <109153512+informaticosonline@users.noreply.github.com>
2023-12-24 12:57:47 +0100

    Initial commit

---
+++ LogDialog.py
@@ -0,0 +1,106 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import os,sys
+
+def resource_path(relative_path):
+        """ Obtener el camino absoluto al recurso,
+        funciona para el desarrollo y para el ejecutable único 
+        """
+        try:
+            # PyInstaller crea un directorio temporal y almacena el path en _MEIPASS
+            base_path = sys._MEIPASS
+        except Exception:
+            base_path = os.path.abspath(".")
+
+        return os.path.join(base_path, relative_path)
+
+class Ui_LoginDialog(object):
+    
+
+    def setupUi(self, LoginDialog):
+        LoginDialog.setObjectName("LoginDialog")
+        LoginDialog.resize(300, 220)
+
+        icono_ruta = resource_path("Studium.png")
+        icono = QtGui.QIcon(icono_ruta)
+        LoginDialog.setWindowIcon(icono)
+
+        # Configurar imagen de fondo
+        fondo_ruta = resource_path("ImgCurso.png")
+        self.background_pixmap = QtGui.QPixmap(fondo_ruta)
+        self.background_label = QtWidgets.QLabel(LoginDialog)
+        self.background_label.setGeometry(QtCore.QRect(0, 0, 300, 220))
+
+        # Aplicar efecto de opacidad
+        opacity_effect = QtWidgets.QGraphicsOpacityEffect()
+        opacity_effect.setOpacity(0.5)  # Ajusta este valor según sea necesario
+        self.background_label.setGraphicsEffect(opacity_effect)
+
+        self.background_label.setPixmap(self.background_pixmap)
+        self.background_label.setScaledContents(True)
+
+        self.username_label = QtWidgets.QLabel(LoginDialog)
+        self.username_label.setGeometry(QtCore.QRect(50, 50, 100, 30))
+        font = QtGui.QFont()
+        font.setFamily("ELEGANT TYPEWRITER")
+        font.setPointSize(12)
+        font.setItalic(False)
+        self.username_label.setFont(font)
+        self.username_label.setObjectName("username_label")
+
+        self.username_input = QtWidgets.QLineEdit(LoginDialog)
+        self.username_input.setGeometry(QtCore.QRect(150, 50, 100, 30))
+        self.username_input.setObjectName("username_input")
+
+        self.password_label = QtWidgets.QLabel(LoginDialog)
+        self.password_label.setGeometry(QtCore.QRect(50, 100, 100, 30))
+        font.setFamily("ELEGANT TYPEWRITER")
+        font.setPointSize(12)
+        font.setItalic(False)
+        self.password_label.setFont(font)
+        self.password_label.setObjectName("password_label")
+
+        self.password_input = QtWidgets.QLineEdit(LoginDialog)
+        self.password_input.setGeometry(QtCore.QRect(150, 100, 100, 30))
+        self.password_input.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.password_input.setObjectName("password_input")
+
+        self.login_button = QtWidgets.QPushButton(LoginDialog)
+        self.login_button.setGeometry(QtCore.QRect(100, 150, 100, 50))
+        
+        self.login_button.setObjectName("login_button")
+
+        self.retranslateUi(LoginDialog)
+        QtCore.QMetaObject.connectSlotsByName(LoginDialog)
+
+    def retranslateUi(self, LoginDialog):
+        _translate = QtCore.QCoreApplication.translate
+        LoginDialog.setWindowTitle(_translate("LoginDialog", "Login"))
+        self.username_label.setText(_translate("LoginDialog", "Usuario:"))
+        self.password_label.setText(_translate("LoginDialog", "Contraseña:"))
+        self.login_button.setText(_translate("LoginDialog", "Login"))
+
+
+class LoginDialog(QtWidgets.QDialog, Ui_LoginDialog):
+    def __init__(self, db, parent=None):
+        super(LoginDialog, self).__init__(parent)
+        self.setupUi(self)
+        self.db = db
+        self.login_button.clicked.connect(self.check_credentials)
+
+    def check_credentials(self):
+        username = self.username_input.text()
+        password = self.password_input.text()
+
+        cursor = self.db.cursor()
+        cursor.execute("SELECT * FROM Claves WHERE nombre = %s AND clave = %s", (username, password))
+        result = cursor.fetchone()
+
+        if result is not None:
+            self.accept()
+        else:
+            QtWidgets.QMessageBox.warning(self, "Login error", "Incorrect username or password.")
+
+
+
+
+
---
+++ WebpRediLogin.py
@@ -0,0 +1,106 @@
+from tkinter import filedialog, simpledialog, messagebox, Tk, Button, PhotoImage
+from PIL import Image
+import tkinter as tk
+import sys, os
+import mariadb
+from PyQt5 import QtWidgets
+from LogDialog import LoginDialog  # Asegúrate de que esta importación sea correcta
+
+def seleccionar_y_procesar_imagen():
+    # Abre un diálogo para seleccionar la imagen
+    file_path = filedialog.askopenfilename(filetypes=[("Imágenes", "*.png;*.jpg;*.jpeg;*.webp")])
+    if file_path:
+        # Se piden las dimensiones de la imagen
+        ancho, alto = solicitar_dimensiones()
+        
+        # Llama a la función para procesar la imagen
+        convertir_y_redimensionar_imagen(file_path, ancho, alto)
+
+def resource_path(relative_path):
+    """ Obtener el camino absoluto al recurso, funciona para el desarrollo y para el ejecutable único """
+    try:
+        # PyInstaller crea un directorio temporal y almacena el path en _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+def solicitar_dimensiones():
+    # Se pide al usuario los valores de ancho y alto, se perimten valores vacios.
+    ancho = simpledialog.askstring("Ancho", "Dame el ancho (en blanco valor original):")
+    alto = simpledialog.askstring("Alto", "Dame el alto (en blanco valor original):")
+
+    try:
+        if ancho:
+            ancho = int(ancho)
+        else:
+            ancho = None
+        # Otra forma de sintaxis de condición, operador ternario
+        alto = int(alto) if alto else None
+
+    except ValueError:
+        print("Error en los tamaños.")
+        messagebox.showerror("Error", "Las dimensiones no son correctas" + "\nSe mantendrá el tamaño original")
+        return None, None
+    
+    return ancho, alto
+
+def convertir_y_redimensionar_imagen(ruta_entrada, ancho, alto):
+    with Image.open(ruta_entrada) as img:
+        # Cuando las dimensiones son válidas redimensiona
+        if ancho and alto:
+            img = img.resize((ancho, alto))
+
+        # Pasa la imagen a WEBP
+        ruta_salida = filedialog.asksaveasfilename(defaultextension=".webp", filetypes=[("WEBP", "*.webp")])
+        if ruta_salida:
+            img.save(ruta_salida, format="webp")
+            print("Archivo WEBP guardado en:", ruta_salida)
+
+def mostrar_ventana_principal():
+    app = Tk()
+    app.title("Redimensión y conversión a WEBP")
+    app.geometry("400x200")
+    app.eval('tk::PlaceWindow . center')
+    app.resizable(False, False)
+
+    icono_ruta = resource_path("Studium.png")
+    fondo_ruta = resource_path("ImgCurso.png")
+
+    icono = PhotoImage(file=icono_ruta)
+    background_image = PhotoImage(file=fondo_ruta)
+
+    background_label = tk.Label(app, image=background_image)
+    background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    select_button = Button(app, text="Selecciona una imagen", command=seleccionar_y_procesar_imagen)
+    select_button.pack(pady=20)
+
+    app.iconphoto(True,icono)
+    app.mainloop()
+
+def main():
+    # Configurar la conexión a la base de datos
+    try:
+        db = mariadb.connect(
+            host= "informaticosonline.es",
+            user="ofluqcym_UsuLicencias",
+            password="#JACL1970",
+            database= "ofluqcym_Licencias"
+        )
+
+    except mariadb.Error as e:
+        print(f"Error conectando a MariaDB: {e}")
+        sys.exit(1)
+
+    # Crear y mostrar el diálogo de inicio de sesión
+    app = QtWidgets.QApplication(sys.argv)
+    login_dialog = LoginDialog(db)
+    if login_dialog.exec_() == QtWidgets.QDialog.Accepted:
+        mostrar_ventana_principal()
+
+if __name__ == "__main__":
+    main()
+
